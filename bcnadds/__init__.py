00a4af88ac74587be72dbfcb17ad9f310ac01287
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-10-14 15:31:02 +0530

    Update __init__.py

--- bcnadds/__init__.py
+++ bcnadds/__init__.py
@@ -1,1 +1,13 @@
-from bcnadds.funcs import customize
+#blackcatuserbot addons
+
+__version__ = "0.0.18"
+__license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
+__copyright__ = "Copyright (C) 2023-present bcncalling <https://github.com/bcncalling>"
+
+from .funcs import customize
+
+def bcnadds():
+  print(
+    "Black cat installed successfully" + "\n"
+    f"Version : {__version__}"
+  )
