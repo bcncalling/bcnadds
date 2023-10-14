b2a7a89b6b139e56edc1d2d48b50be8c7a09619c
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-10-14 11:18:26 +0530

    Update funcs.py

--- bcnadds/funcs.py
+++ bcnadds/funcs.py
@@ -2,14 +2,15 @@ import random
 import os
 import asyncio
 from pyrogram import Client
+from https://github.com/bcncalling/blackcatuserbot/blob/main/bcnplugs import app, client
 
 async def customize():
     rem = None
     try:
-        chat_id = "LOG_CHANNEL" 
+        chat_id = "-1001916479883" 
         if app.get_me().photo:
             return
-        print("Customizing Your Assistant Bot in @BOTFATHER")
+        print("Customizing Your Assistant Bot in @Botfather")
         UL = f"@{app.get_me().username}"
         bcn = app.get_me()
         if not bcn.username:
@@ -17,48 +18,35 @@ async def customize():
         else:
             sir = f"@{bcn.username}"
         
-        file = random.choice(
-            [
-                "https://telegra.ph/file/92cd6dbd34b0d1d73a0da.jpg",
-                "https://telegra.ph/file/a97973ee0425b523cdc28.jpg",
-                "resources/extras/ultroid_assistant.jpg",
-            ]
-        )
-        
-        if not os.path.exists(file):
-            # You need to implement a download_file function to download the file
-            # file = await download_file(file, "profile.jpg")
-            rem = True
-        
         msg = await app.send_message(chat_id, "**Auto Customisation** Started on @Botfather")
         await asyncio.sleep(1)
         
         # Send commands to BotFather
-        await bot.send_message("botfather", "/cancel")
+        await client.send_message("botfather", "/cancel")
         await asyncio.sleep(1)
-        await bot.send_message("botfather", "/setuserpic")
+        await client.send_message("botfather", "/setuserpic")
         await asyncio.sleep(1)
-        isdone = (await bot.get_history("botfather", limit=1))[-1].text
+        isdone = (await client.get_history("botfather", limit=1))[-1].text
         
         if isdone.startswith("Invalid bot"):
             print("Error while trying to customize assistant, skipping...")
             return
         
-        await bot.send_message("botfather", UL)
+        await client.send_message("botfather", UL)
         await asyncio.sleep(1)
-        await bot.send_document("botfather", file)
+        await client.send_document("botfather", file)
         await asyncio.sleep(2)
-        await bot.send_message("botfather", "/setabouttext")
+        await client.send_message("botfather", "/setabouttext")
         await asyncio.sleep(1)
-        await bot.send_message("botfather", UL)
+        await client.send_message("botfather", UL)
         await asyncio.sleep(1)
-        await bot.send_message("botfather", f"✨ Hello ✨!! I'm Assistant Bot of {sir}")
+        await client.send_message("botfather", f"✨ Hello ✨!! I'm Assistant Bot of {sir}")
         await asyncio.sleep(2)
-        await bot.send_message("botfather", "/setdescription")
+        await client.send_message("botfather", "/setdescription")
         await asyncio.sleep(1)
-        await bot.send_message("botfather", UL)
+        await client.send_message("botfather", UL)
         await asyncio.sleep(1)
-        await bot.send_message("botfather", f"Developed and designed by @Blackcatfighters & @blackcatserver")
+        await client.send_message("botfather", f"Developed and designed by @Blackcatfighters & @blackcatserver")
         await asyncio.sleep(2)
         await msg.edit("Completed **Auto Customisation** at @BotFather.")
         if rem:
