7b0dd73455f2090618065ce6e9e5d7ac26573b69
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-12-05 20:35:54 +0530

    Update __init__.py

--- bcnadds/__init__.py
+++ bcnadds/__init__.py
@@ -5,6 +5,7 @@ __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2023-present bcncalling <https://github.com/bcncalling>"
 
 from .funcs import customize
+from .TgGraph import TgGraph
 
 def bcnadds():
   print(
