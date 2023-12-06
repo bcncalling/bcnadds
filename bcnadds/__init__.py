8b9f1bcde7e6dca4a7fe232c1d86870b3bd8c9b9
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-12-06 10:41:05 +0530

    Update __init__.py

--- bcnadds/__init__.py
+++ bcnadds/__init__.py
@@ -1,6 +1,6 @@
 #blackcatuserbot addons
 
-__version__ = "0.0.22"
+__version__ = "0.0.23"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2023-present bcncalling <https://github.com/bcncalling>"
 
