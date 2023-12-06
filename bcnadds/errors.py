8f81b48b211a8e162ad4480c45c318f717256626
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-12-06 10:29:14 +0530

    Create errors.py

---
+++ bcnadds/errors.py
@@ -0,0 +1,20 @@
+class TelegraphException(Exception):
+    pass
+
+
+class ParsingException(Exception):
+    pass
+
+
+class NotAllowedTag(ParsingException):
+    pass
+
+
+class InvalidHTML(ParsingException):
+    pass
+
+
+class RetryAfterError(TelegraphException):
+    def __init__(self, retry_after: int):
+        self.retry_after = retry_after
+        super().__init__(f'Telegram says floodwait. Retry in {retry_after} seconds')
